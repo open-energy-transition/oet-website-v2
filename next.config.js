423af4176540101753814abea7a72247fc440a76
Akshat Mittal <akshatmittal.0007@gmail.com>
2024-11-22 20:36:38 +0700

    updating variable name

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
