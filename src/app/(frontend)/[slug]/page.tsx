7b178d0981f1a71da87528941f46950426fd2c58
Akshat Mittal <akshatmittal.0007@gmail.com>
2024-11-25 17:02:51 +0700

    removing pages

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -49,25 +49,25 @@ type Args = {
 }
 
 export default async function Page({ params: paramsPromise }: Args) {
-  const { slug = 'home' } = await paramsPromise
-  const url = '/' + slug
+  // const { slug = 'home' } = await paramsPromise
+  // const url = '/' + slug
 
-  let page: PageType | null
+  // let page: PageType | null
 
-  page = await queryPageBySlug({
-    slug,
-  })
+  // page = await queryPageBySlug({
+  //   slug,
+  // })
 
-  // Remove this code once your website is seeded
-  if (!page && slug === 'home') {
-    page = homeStatic
-  }
+  // // Remove this code once your website is seeded
+  // if (!page && slug === 'home') {
+  //   page = homeStatic
+  // }
 
-  if (!page) {
-    return <PayloadRedirects url={url} />
-  }
+  // if (!page) {
+  //   return <PayloadRedirects url={url} />
+  // }
 
-  const { hero, layout } = page
+  // const { hero, layout } = page
 
   return (
     <>
