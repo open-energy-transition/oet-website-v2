91c55aee5a8bc18060290a8d750f46abe98a9687
jacek-oet <jacek.bendig@openenergytransition.org>
2025-08-22 12:09:27 +0200

    fix: force dynamic

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,8 @@
 import PageTemplate, { generateMetadata } from './[slug]/page'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default PageTemplate
 
 export { generateMetadata }
